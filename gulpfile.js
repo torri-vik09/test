62b65be8ea7b90c57ff1fd60eeb956ce28485dd7
Torri <torri1.torri1.9@gmail.com>
2019-11-03 18:41:06 +0200

    First page

--- gulpfile.js
+++ gulpfile.js
@@ -64,9 +64,9 @@ gulp.task('html:build', function () {
     return gulp.src(path.src.html) // выбор всех pug файлов по указанному пути
         .pipe(plumber()) // отслеживание ошибок
         .pipe(rigger()) // импорт вложений
-        .pipe(pug({ pretty: true }))
+        .pipe(pug({pretty: true}))
         .pipe(gulp.dest(path.build.html)) // выкладывание готовых файлов
-        .pipe(webserver.reload({ stream: true })); // перезагрузка сервера
+        .pipe(webserver.reload({stream: true})); // перезагрузка сервера
 });
 
 // сбор стилей
@@ -77,11 +77,11 @@ gulp.task('css:build', function () {
         .pipe(sass()) // scss -> css
         .pipe(autoprefixer()) // добавим префиксы
         .pipe(gulp.dest(path.build.css))
-        .pipe(rename({ suffix: '.min' }))
+        .pipe(rename({suffix: '.min'}))
         .pipe(cleanCSS()) // минимизируем CSS
         .pipe(sourcemaps.write('./')) // записываем sourcemap
         .pipe(gulp.dest(path.build.css)) // выгружаем в build
-        .pipe(webserver.reload({ stream: true })); // перезагрузим сервер
+        .pipe(webserver.reload({stream: true})); // перезагрузим сервер
 });
 
 // сбор js
@@ -90,40 +90,45 @@ gulp.task('js:build', function () {
         .pipe(plumber()) // для отслеживания ошибок
         .pipe(rigger()) // импортируем все указанные файлы в main.js
         .pipe(gulp.dest(path.build.js))
-        .pipe(rename({ suffix: '.min' }))
+        .pipe(rename({suffix: '.min'}))
         .pipe(sourcemaps.init()) //инициализируем sourcemap
         .pipe(uglify()) // минимизируем js
         .pipe(sourcemaps.write('./')) //  записываем sourcemap
         .pipe(gulp.dest(path.build.js)) // положим готовый файл
-        .pipe(webserver.reload({ stream: true })); // перезагрузим сервер
+        .pipe(webserver.reload({stream: true})); // перезагрузим сервер
 });
 
-// перенос шрифтов
 gulp.task('fonts:build', function () {
-    return gulp.src(path.src.fonts)
+    return gulp.src([
+        'node_modules/@fortawesome/fontawesome-free/**',
+        '!node_modules/@fortawesome/fontawesome-free/**/*.map',
+        '!node_modules/@fortawesome/fontawesome-free/.npmignore',
+        '!node_modules/@fortawesome/fontawesome-free/*.txt',
+        '!node_modules/@fortawesome/fontawesome-free/*.md',
+        '!node_modules/@fortawesome/fontawesome-free/*.json'
+    ])
         .pipe(gulp.dest(path.build.fonts));
 });
-
 
 // обработка картинок
 gulp.task('image:build', function () {
     return gulp.src(path.src.img) // путь с исходниками картинок
         .pipe(cache(imagemin([ // сжатие изображений
-            imagemin.gifsicle({ interlaced: true }),
+            imagemin.gifsicle({interlaced: true}),
             jpegrecompress({
                 progressive: true,
                 max: 90,
                 min: 80
             }),
             pngquant(),
-            imagemin.svgo({ plugins: [{ removeViewBox: false }] })
+            imagemin.svgo({plugins: [{removeViewBox: false}]})
         ])))
         .pipe(gulp.dest(path.build.img)); // выгрузка готовых файлов
 });
 
 // удаление каталога build 
 gulp.task('clean:build', function () {
-    return gulp.src(path.clean, { read: false })
+    return gulp.src(path.clean, {read: false})
         .pipe(rimraf());
 });
 
@@ -157,5 +162,5 @@ gulp.task('watch', function () {
 // задача по умолчанию
 gulp.task('default', gulp.series(
     'build',
-    gulp.parallel('webserver','watch')      
+    gulp.parallel('webserver', 'watch')
 ));
